9c1da909412ffae76c104d9b41ca49dd6037b087
rmprieto <COLD_BITTER@movistar.es>
2024-08-21 20:11:43 +0200

    Create ejercicio4.js

---
+++ ejercicio4.js
@@ -0,0 +1,27 @@
+const cartoons = [
+    { name: 'Mickey Mouse', debut: 1928 },
+    { name: 'Bugs Bunny', debut: 1938 },
+    { name: 'SpongeBob SquarePants', debut: 1999 },
+    { name: 'Tom and Jerry', debut: 1940 },
+    { name: 'Scooby-Doo', debut: 1969 },
+    { name: 'The Flintstones', debut: 1960 },
+    { name: 'Batman: The Animated Series', debut: 1992 },
+    { name: 'The Simpsons', debut: 1989 },
+    { name: 'Pokémon', debut: 1997 },
+    { name: "Dexter's Laboratory", debut: 1996 }
+  ];
+  
+// Añade tu código de bucle aquí
+let olderCartoon = 0;
+let olderDebut = 0
+
+for (let i = 0; i < cartoons.length; i++){
+    const cartoon = cartoons[i];
+
+    if (cartoon.debut > olderDebut) {
+      olderCartoon = cartoon.name;
+      olderDebut = cartoon.debut;
+    }
+}
+
+console.log('La pelicula más antigua es: ' + olderCartoon + ' y es del año: ' + olderDebut);
